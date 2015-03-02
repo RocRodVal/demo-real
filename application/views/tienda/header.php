dc37c2730dcf6a840f851d609907fe27a5ccf720
Daniel Carro Fernández <dcarro@altabox.net>
2015-03-02 13:47:44 +0100

    Añadidos los JS para IE en local

--- application/views/tienda/header.php
+++ application/views/tienda/header.php
@@ -18,8 +18,8 @@
     <?= Xcrud::load_css(); ?>
     <?= Xcrud::load_js(); ?>
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/explorer/html5shiv.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>assets/js/explorer/respond.min.js" type="text/javascript"></script>
     <![endif]-->
     <link rel="shortcut icon" href="<?= site_url('favicon.ico') ?>">
     <!-- Carga de los JS/CSS dependiendo de donde estemos-->
