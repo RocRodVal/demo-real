906579ff5c0c9f91fb795e5be5e9ba379bf36e9d
Gustavo Zapico <g.zapico@netavanza.com>
2015-05-12 09:51:27 +0200

    Update BBDD

--- _bbdd/plataformas.sql
+++ _bbdd/plataformas.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: May 08, 2015 at 05:58 PM
+-- Generation Time: May 12, 2015 at 09:49 AM
 -- Server version: 5.5.40
 -- PHP Version: 5.4.34
 
