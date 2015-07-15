c5a082356fe272d2e13b17b651d444bc1dbe39af
David Bourgon <dbourgon@altabox.net>
2015-07-15 15:41:22 +0200

    Fichero de la vista "mantenimiento"

---
+++ application/views/common/mantenimiento.php
@@ -0,0 +1,27 @@
+		<!-- #container -->
+		<div id="bg">
+  			<img src="<?=site_url('assets/images/'.$bg_image)?>" alt="">
+		</div>
+	    <div class="container">
+	        <div class="row">
+	            <div class="col-md-4 col-md-offset-4">
+	                <div class="login-panel panel panel-default">
+	                    <div class="panel-heading">
+	                        <center><img src="<?=site_url('assets/images/logo-orange_big.png')?>" title="<?=lang('comun.titulo')?>" width="50%" /></center>
+	                    </div>
+	                    <div class="panel-body">
+	                        <form action="#" method="post" class="content_auto form_login">
+
+	                        	<div id="infoMessage" class="mantenimiento">
+                                    <h2>Parada por mantenimiento</h2>
+                                    <p class="desc">El presente servicio se encuentra en parada por mantenimiento.</p>
+                                    <p class="desc">Procederemos a la reanudación del mismo el próximo: <strong>Lunes, 13 de Julio, a las 8:00h</strong></p>
+                                </div>
+
+	                        </form>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	    </div>    
+		<!-- /#container -->		
