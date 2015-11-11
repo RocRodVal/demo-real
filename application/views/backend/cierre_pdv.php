36efd561b6cff9bdb1a6048c65d18ccc6d00ba75
David Bourgon <dbourgon@altabox.net>
2015-11-11 11:56:07 +0100

    Actualización de la baja de SFID a la bloominización y añadido un historico para poder recuperar.

--- application/views/backend/cierre_pdv.php
+++ application/views/backend/cierre_pdv.php
@@ -21,10 +21,37 @@
                 	<form action="<?=site_url('admin/cierre_pdv');?>" method="post" class="form-inline form-sfid">
                         <div class="form-group">
                             <label>SFID</label>
-                            <input class="form-control" placeholder="SFID" name="sfid" id="sfid">
+                            <input class="form-control" placeholder="SFID" name="sfid" id="sfid" value="<?=($baja_sfid!='') ? $baja_sfid : ''?>">
                             <button type="submit" class="btn btn-default">Buscar</button>
                         </div>
                     </form>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <?php if(!is_null($error)) {
+
+                        echo (!$error)
+                            ? '<p class="message success"><i class="glyphicon glyphicon-error"></i>'
+                            : '<p class="message error"><i class="glyphicon glyphicon-error"></i>';
+
+
+                        switch($error)
+                        {
+                            case FALSE:
+                                echo "El SFID <strong>$baja_sfid</strong> se ha dado de baja correctamente.";
+                                break;
+
+                            case "incidencias":
+                                echo "Debes cerrar las incidencias abiertas del SFID <strong>$baja_sfid</strong> antes de poder darlo de baja";
+                                break;
+
+                            default: break;
+                        }
+
+                        echo '</p>';
+                    } ?>
                 </div>
             </div>
             <?php 
@@ -38,7 +65,7 @@
                     <div class="col-lg-12">
                         <p>&nbsp;</p>
                         <p class="message warning"><i class="glyphicon glyphicon-warning-sign"></i> Se va a proceder al borrado de dispositivos, muebles y punto de servicio de la app.</p>
-                        <p class="message"><a href="<?=site_url('admin/get_inventarios_sfid/'.$baja_sfid.'/baja')?>"><i class="fa fa-file-pdf-o"></i>  Descargar informe de baja</a></p>
+                        <p class="message"><a href="<?=site_url('admin/get_inventarios_sfid/'.$baja_sfid.'/baja/'.$id_pds)?>"><i class="fa fa-file-pdf-o"></i>  Descargar informe de baja</a></p>
                         <p>&nbsp;</p>
                     </div>
                 </div>
@@ -62,7 +89,9 @@
                                             <th>Ref.</th>
                                             <th>SFID</th>
                                             <th>Tipo</th>
-                                            <th>Panelado</th>
+                                            <th>Subtipo</th>
+                                            <th>Segmento</th>
+                                            <th>Tipología</th>
                                             <th>Nombre comercial</th>
                                             <th>Territorio</th>
                                             <th>Operaciones</th>
@@ -79,8 +108,10 @@
     									<tr>
     										<td><?php echo $tienda->id_pds ?></td>
     										<td><?php echo $tienda->reference ?></a></td>
-    										<td><?php echo $tienda->pds ?></td>
-    										<td><?php echo $tienda->panelado ?></td>
+    										<td><?php echo $tienda->tipo ?></td>
+                                            <td><?php echo $tienda->subtipo ?></td>
+    										<td><?php echo $tienda->segmento ?></td>
+                                            <td><?php echo $tienda->tipologia ?></td>
     										<td><?php echo $tienda->commercial ?></td>
     										<td><?php echo $tienda->territory ?></td>
     										<td><button type="submit" class="btn btn-default">Revisar y cerrar</button></td>
