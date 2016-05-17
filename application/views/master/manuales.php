713a21a705add21f11e112c3129b1149cbd37c35
Esther <esecades@altabox.net>
2016-05-17 12:02:30 +0200

    manual de usuario Smart Store

--- application/views/master/manuales.php
+++ application/views/master/manuales.php
@@ -12,6 +12,7 @@
                     <a href="<?php echo base_url(); ?>assets/manuales/manual_incidencia_de_alarmado_iphone.pdf" target="_blank">Manual incidencia de alarmado iPhone</a><br />
                     <a href="<?php echo base_url(); ?>assets/manuales/manual_desactivacion_alarma_tablet_lg_gpad_8_0.pdf" target="_blank">Manual desactivación alarma Tablet LG GPAD 8.0</a><br />
                     <a href="<?php echo base_url(); ?>assets/manuales/manual_sustitucion_cable_carga-iphone_tablets.pdf" target="_blank">Manual sustitución cable de carga iPhone y Tablets</a><br />
+                    <a href="<?php echo base_url(); ?>assets/manuales/manual_usuario_gsp-SmartStore.pdf" target="_blank">Manual sistemas de seguridad Smart Store</a><br />
                 </div>
             </div>
         </div>
