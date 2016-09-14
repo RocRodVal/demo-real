26dc7b8c28e4be3db252dd8114103a2891a4e1e5
Esther <esecades@altabox.net>
2016-09-14 10:18:16 +0200

    Consulta para la visualización de incidencias abiertas y cerradas

--- application/models/incidencia_model.php
+++ application/models/incidencia_model.php
@@ -267,7 +267,7 @@ class Incidencia_model extends CI_Model {
  */
 
                  ->join('pds','incidencias.id_pds = pds.id_pds','left outer')
-                 ->join('intervenciones_incidencias','intervenciones_incidencias.id_incidencia= incidencias.id_incidencia','left')
+                // ->join('intervenciones_incidencias','intervenciones_incidencias.id_incidencia= incidencias.id_incidencia','left')
                  ->join('pds_supervisor','pds.id_supervisor= pds_supervisor.id','left')
                  ->join('province','pds.province= province.id_province','left')
                  ->join('displays_pds','incidencias.id_displays_pds= displays_pds.id_displays_pds','left outer')
@@ -275,8 +275,8 @@ class Incidencia_model extends CI_Model {
                  ->join('devices_pds','incidencias.id_devices_pds=devices_pds.id_devices_pds','left outer')
                  ->join('device','devices_pds.id_device=device.id_device','left outer')
                  ->join('territory','territory.id_territory=pds.territory','left outer')
-                 ->join('brand_device','device.brand_device = brand_device.id_brand_device','left outer')
-                 ->join('type_incidencia','type_incidencia.id_type_incidencia=incidencias.id_type_incidencia','left outer');
+                 ->join('brand_device','device.brand_device = brand_device.id_brand_device','left outer');
+                 //->join('type_incidencia','type_incidencia.id_type_incidencia=incidencias.id_type_incidencia','left outer');
 
 
 
@@ -293,13 +293,19 @@ class Incidencia_model extends CI_Model {
         if(isset($filtros["id_provincia"])  && !empty($filtros["id_provincia"]))    $this->db->where('province.id_province',$filtros['id_provincia']);
 
         if(isset($filtros["reference"])     && !empty($filtros["reference"]))       $this->db->where('reference',$filtros['reference']);
-        if(isset($filtros["id_intervencion"])&& !empty($filtros["id_intervencion"]))$this->db->where('intervenciones_incidencias.id_intervencion',$filtros['id_intervencion']);
+        if(isset($filtros["id_intervencion"])&& !empty($filtros["id_intervencion"])) {
+            $this->db->join('intervenciones_incidencias','intervenciones_incidencias.id_incidencia= incidencias.id_incidencia','left');
+            $this->db->where('intervenciones_incidencias.id_intervencion', $filtros['id_intervencion']);
+        }
 
         if(isset($filtros["id_tipo"])       && !empty($filtros["id_tipo"]))         $this->db->where('pds.id_tipo',$filtros['id_tipo']);
         if(isset($filtros["id_subtipo"])    && !empty($filtros["id_subtipo"]))      $this->db->where('pds.id_subtipo',$filtros['id_subtipo']);
         if(isset($filtros["id_segmento"])   && !empty($filtros["id_segmento"]))     $this->db->where('pds.id_segmento',$filtros['id_segmento']);
         if(isset($filtros["id_tipologia"])  && !empty($filtros["id_tipologia"]))    $this->db->where('pds.id_tipologia',$filtros['id_tipologia']);
-        if(isset($filtros["id_tipo_incidencia"])  && !empty($filtros["id_tipo_incidencia"]))    $this->db->where('incidencias.id_type_incidencia',$filtros['id_tipo_incidencia']);
+        if(isset($filtros["id_tipo_incidencia"])  && !empty($filtros["id_tipo_incidencia"])) {
+            $this->db->join('type_incidencia','type_incidencia.id_type_incidencia=incidencias.id_type_incidencia','left outer');
+            $this->db->where('incidencias.id_type_incidencia',$filtros['id_tipo_incidencia']);
+        }
 
 
 
@@ -322,7 +328,7 @@ class Incidencia_model extends CI_Model {
 
         $query =   $this->db->get('incidencias',$cfg_pagination['per_page'], ($page-1) * $cfg_pagination['per_page']);
          //
-       // echo $this->db->last_query();
+        echo "<br><br>".$this->db->last_query();
       //  exit;
 
         return $query->result();
@@ -717,7 +723,7 @@ class Incidencia_model extends CI_Model {
         /* Obtener el resultado */
         $query =  $this->db->get('incidencias')->row();
 
-      //  echo $this->db->last_query();
+        echo $this->db->last_query();
 
         return $query->cantidad;
 
