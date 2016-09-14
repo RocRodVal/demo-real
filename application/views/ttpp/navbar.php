13eef4da0cdedb2c0c9476bb6f4aa4752fb4231a
Esther <esecades@altabox.net>
2016-09-14 12:59:04 +0200

    Menu de navegación para el usuario tiendas propias y territorial

--- application/views/ttpp/navbar.php
+++ application/views/ttpp/navbar.php
@@ -29,7 +29,7 @@
                         {
                         ?>
 
-                            <?php $estado_incidencias = array("estado_incidencias");
+                            <?php $estado_incidencias = array("estado_incidencias","detalle_incidencia");
                             $estado_incidencias_inner = array();
                             ?>
 
