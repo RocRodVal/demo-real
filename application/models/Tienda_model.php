d0c0071bc27998920927a3d96727d1b74c929efd
Esther <esecades@altabox.net>
2021-03-18 13:22:48 +0100

    Las tiendas que pueden realizar pedidos son aquellas cuya tipología (subtipo) es SMARTSTORE o EVOLUCION NEXT

--- application/models/Tienda_model.php
+++ application/models/Tienda_model.php
@@ -3105,9 +3105,12 @@ class Tienda_model extends CI_Model {
                     $fecha = date('Y-m-d H:i:s');
                     $position = $terminal->position;
                     $id_device = $terminal->id_device;
-                    $SQL = " INSERT INTO devices_pds(client_type_pds,id_pds, id_displays_pds, id_display, alta,position, id_device, status)
+                    $id_muebledisplay = 0;
+                    if($terminal->isDisplay)
+                        $id_muebledisplay = $terminal->id_muebledisplay;
+                    $SQL = " INSERT INTO devices_pds(client_type_pds,id_pds, id_displays_pds, id_display, alta,position, id_device,id_muebledisplay, status)
                                             VALUES(".$client_type_pds.",".$id_pds.",".$id_displays_pds.",".$id_display.",'".
-                        $fecha."',".$position.",".$id_device.",'Alta'); ";
+                        $fecha."',".$position.",".$id_device.",".$id_muebledisplay.",'Alta'); ";
                     $this->db->query($SQL);
 
                     /*Insertar en el historico de tienda el estado del dispositivo*/
